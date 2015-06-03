622465e4661462666f922bf9ddae0ae087e6fb6a
LavanyaPuram <lavanya.puram90@gmail.com>
2015-06-03 18:51:58 +0100

    First Commit

---
+++ EmployeeService/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeService
+{
+    public class Employee
+    {
+        #region Private class fields
+            private int _id;
+            private string _name;
+            private string _gender;
+            private DateTime _dob;
+        #endregion
+        #region Public properties
+            public int Id
+            {
+                get { return _id; }
+                set { _id = value; }
+            }
+            public string Name
+            {
+                get { return _name; }
+                set { _name = value; }
+            }
+            public string Gender
+            {
+                get { return _gender; }
+                set { _gender = value; }
+            }
+            public DateTime DOB
+            {
+                get { return _dob; }
+                set { _dob = value; }
+            }
+        #endregion
+    }
+    
+}
+
---
+++ EmployeeService/EmployeeService.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+namespace EmployeeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in both code and config file together.
+    public class EmployeeService : IEmployeeService
+    {
+
+        public Employee GetEmployee(int Id)
+        {
+            Employee employee = new Employee();
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SP_GetEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter p = new SqlParameter("@Id", Id);
+            cmd.Parameters.Add(p);
+            if(con.State!=ConnectionState.Open)
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                employee.Id = Convert.ToInt32(dr["Id"]);
+                employee.Name = dr["Name"].ToString();
+                employee.Gender = dr["Gender"].ToString();
+                employee.DOB = Convert.ToDateTime(dr["DOB"]);
+            }
+            return employee;
+        }
+
+        public void SaveEmployee(Employee employee)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SP_SaveEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter[] p = new SqlParameter[]
+            {
+               new SqlParameter("@Id",employee.Id),
+               new SqlParameter("@Name",employee.Name),
+               new SqlParameter("@Gender",employee.Gender),
+               new SqlParameter("@DOB",employee.DOB)
+            };
+            cmd.Parameters.AddRange(p);
+            if (con.State != ConnectionState.Open)
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        public void UpdateEmployee(Employee employee)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SP_UpdateEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter[] p = new SqlParameter[]
+            {
+               new SqlParameter("@Id",employee.Id),
+               new SqlParameter("@Name",employee.Name),
+               new SqlParameter("@Gender",employee.Gender),
+               new SqlParameter("@DOB",employee.DOB)
+            };
+            cmd.Parameters.AddRange(p);
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+
+
+        public void DeleteEmployee(int Id)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conStr"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("SP_DeleteEmployee", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlParameter p = new SqlParameter("@Id", Id);
+            cmd.Parameters.Add(p);
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
